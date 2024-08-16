97385cf4d8349fa0500255c18f37c57dda04a69a
RayenDz <132817343+RayenDz@users.noreply.github.com>
2024-08-16 19:52:20 +0200

    Update js.js

--- RechMap/js.js
+++ RechMap/js.js
@@ -7,3 +7,18 @@ document.addEventListener('keydown', function(e) {
         e.preventDefault();
     }
 });
+document.onkeydown = function(e) {
+    if (e.ctrlKey && (e.key === 'u' || e.key === 's' || e.key === 'c' || e.key === 'p')) {
+        e.preventDefault();
+    }
+};
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+document.getElementById('textSearch').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // منع إرسال النموذج الافتراضي إذا كان داخل نموذج
+        searchData(); // استدعاء دالة البحث
+    }
+});
+
